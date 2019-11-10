1ebe13edb75ff49d8c5c96e4aedc54a67aa0ee35
Charley Wright <charleywright06@gmail.com>
2019-11-10 20:04:28 +0000

    V2 Gui and load file dialog

--- Main.cs
+++ Main.cs
@@ -7,53 +7,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using WindowsInput.Native;
-using WindowsInput;
+using System.IO;
 
-namespace Keystroke_Tool
+namespace Keystroke_Tool_V2
 {
-    public partial class KeystrokeTool : Form
+    public partial class Main : Form
     {
-        public static string input;
-        public static bool enter;
-        public static bool tab;
-        public static InputSimulator Simulator;
-        public static double delay;
-        public static double startDelay;
-        public static int repetitions;
+        public static OpenFileDialog ofd;
 
-        public KeystrokeTool()
+        public Main()
         {
             InitializeComponent();
         }
 
-        private void KeystrokeTool_Load(object sender, EventArgs e)
+        private void EditorBtn_Click(object sender, EventArgs e)
         {
-            
+            Editor editor = new Editor();
+            editor.Show();
         }
 
-        private void GoBtn_Click(object sender, EventArgs e)
+        private void LoadBtn_Click(object sender, EventArgs e)
         {
-            Simulator = new InputSimulator();
+            ofd = new OpenFileDialog();
 
-            try { input = InputField.Text; }
-            catch (NullReferenceException) { input = ""; }
-            try { delay = Convert.ToDouble(waitTimeField.Text); }
-            catch (FormatException) { delay = 0; }
-            catch (NullReferenceException) { delay = 0; }
-            startDelay = Convert.ToDouble(startDelayField.Text);
-            repetitions = Convert.ToInt32(repetitionsField.Text);
-            enter = EnterCheckBox.Checked;
-            tab = TabCheckBox.Checked;
+            ofd.Filter = "KT|*.kt";
 
-
-            Simulator.Keyboard.Sleep(Convert.ToInt32(startDelay * 1000));
-            for (int i = 0; i < repetitions; i++)
+            // Show dialog and put file name into OutputLabel
+            if(ofd.ShowDialog() == DialogResult.OK)
             {
-                Simulator.Keyboard.TextEntry(input);
-                if (enter == true) { Simulator.Keyboard.KeyPress(VirtualKeyCode.RETURN); }
-                if (tab == true) { Simulator.Keyboard.KeyPress(VirtualKeyCode.TAB); }
-                Simulator.Keyboard.Sleep(Convert.ToInt32(delay * 1000));
+                OutputLabel.Text = "Loaded File: " + ofd.SafeFileName;
             }
         }
     }
--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Keystroke_Tool
+namespace Keystroke_Tool_V2
 {
     static class Program
     {
@@ -16,7 +16,7 @@ namespace Keystroke_Tool
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new KeystrokeTool());
+            Application.Run(new Main());
         }
     }
 }
